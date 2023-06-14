24fa720ff26debd7d409d772a062e7e0b233c0c5
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-06-14 13:02:16 -0300

    enum

--- build/module01.js
+++ build/module01.js
@@ -3,3 +3,13 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+//ENUM ------------------------->
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 1] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 2] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 3] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
+let employeeStatus = ContractStatus.Temp;
+console.log(employeeStatus);
+console.log(ContractStatus[employeeStatus]);
--- module01.ts
+++ module01.ts
@@ -2,3 +2,18 @@ function addNumbers(x:number, y:number) {
     return x + y;
   }
   console.log(addNumbers(3, 6));
+
+  
+
+  //ENUM ------------------------->
+
+  enum ContractStatus {
+    Permanent = 1,
+    Temp,
+    Apprentice
+  }
+  
+  let employeeStatus: ContractStatus = ContractStatus.Temp
+  console.log(employeeStatus);
+  
+  console.log(ContractStatus[employeeStatus]);
