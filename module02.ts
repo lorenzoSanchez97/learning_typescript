a8733f2cca7145e9858937882e6f41eb2c975f88
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-06-15 11:51:12 -0300

    Extendiendo interfaces

--- module02.ts
+++ module02.ts
@@ -1,25 +1,42 @@
 //  interfaces ----------------------------------------------->
 
 interface IceCream {
-    flavor: string;
-    scoops: number;
-    instructions?: string;
+  flavor: string;
+  scoops: number;
+  instructions?: string;
+}
+
+// let myIceCream: IceCream = {
+//   flavor: "Chocolate",
+//   scoops: 3
+// }
+
+// console.log(myIceCream.flavor)
+
+function tooManyScoops(dessert: Sundae) {
+  if (dessert.scoops >= 4) {
+    return dessert.scoops + " is too many scoops!"
+  } else {
+    return 'Your order will be ready soon!';
   }
-  
-  let myIceCream: IceCream = {
-    flavor: "Chocolate",
-    scoops: 3,
-    instructions: ""
-  }
-  
-  console.log(myIceCream.flavor)
-  
-  function tooManyScoops(dessert: IceCream) {
-    if (dessert.scoops >= 4) {
-      return dessert.scoops + " is too many scoops!"
-    } else {
-      return 'Your order will be ready soon!';
-    }
-  }
-  
-  console.log(tooManyScoops({flavor: "Chocolate", scoops: 3}))
+}
+
+
+console.log(tooManyScoops({ flavor: "Chocolate", scoops: 5, sauce: "strawberry", nuts: true }))
+
+
+// extendiendo interfaces -------------------------------------------------------->
+
+interface Sundae extends IceCream {
+  sauce: 'chocolate' | 'caramel' | 'strawberry';
+  nuts?: boolean;
+  whippedCream?: boolean;
+  instructions?: string;
+}
+
+let myIceCream: Sundae = {
+  flavor: 'vanilla',
+  scoops: 2,
+  sauce: "strawberry",
+  nuts: true
+}
