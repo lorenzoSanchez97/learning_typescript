ce54fb0e2c26863f024a59d31db8a469636f463a
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-06-23 12:13:58 -0300

    Clases

--- build/module02.js
+++ build/module02.js
@@ -1,11 +1,10 @@
 "use strict";
 //  interfaces ----------------------------------------------->
-let myIceCream = {
-    flavor: "Chocolate",
-    scoops: 3,
-    instructions: ""
-};
-console.log(myIceCream.flavor);
+// let myIceCream: IceCream = {
+//   flavor: "Chocolate",
+//   scoops: 3
+// }
+// console.log(myIceCream.flavor)
 function tooManyScoops(dessert) {
     if (dessert.scoops >= 4) {
         return dessert.scoops + " is too many scoops!";
@@ -14,4 +13,10 @@ function tooManyScoops(dessert) {
         return 'Your order will be ready soon!';
     }
 }
-console.log(tooManyScoops({ flavor: "Chocolate", scoops: 3 }));
+console.log(tooManyScoops({ flavor: "Chocolate", scoops: 5, sauce: "strawberry", nuts: true }));
+let myIceCream = {
+    flavor: 'vanilla',
+    scoops: 2,
+    sauce: "strawberry",
+    nuts: true
+};
---
+++ build/module05.js
@@ -0,0 +1,47 @@
+"use strict";
+class Car {
+    constructor(make, color, doors = 4) {
+        this._make = make;
+        this._color = color;
+        this._doors = doors;
+    }
+    // Accessors
+    get make() {
+        return this._make;
+    }
+    set make(make) {
+        this._make = make;
+    }
+    get color() {
+        return 'The color of the car is ' + this._color;
+    }
+    set color(color) {
+        this._color = color;
+    }
+    get doors() {
+        return this._doors;
+    }
+    set doors(doors) {
+        if ((doors % 2) === 0) {
+            this._doors = doors;
+        }
+        else {
+            throw new Error('Doors must be an even number');
+        }
+    }
+    // Methods
+    // Methods
+    accelerate(speed) {
+        return `${this.worker()} is accelerating to ${speed} MPH.`;
+    }
+    brake() {
+        return `${this.worker()} is braking with the standard braking system.`;
+    }
+    turn(direction) {
+        return `${this.worker()} is turning ${direction}`;
+    }
+    // This function performs work for the other method functions
+    worker() {
+        return this._make;
+    }
+}
