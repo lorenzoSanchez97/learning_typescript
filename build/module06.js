9676e1c9883fa679c1bef188bc6e91b23e4d2564
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-06 13:14:46 -0300

    Genéricos

--- build/module06.js
+++ build/module06.js
@@ -2,16 +2,61 @@
 // interface ProcessIdentity <T, U> {
 //     (value: T, message: U) : T;
 //     }
-class processIdentity {
-    constructor(val, msg) {
-        this.value = val;
-        this.message = msg;
-    }
-    process() {
-        console.log(this.message);
-        return this.value;
+//     function processIdentity<T, U> (value: T, message: U) : T {
+//         console.log(message);
+//         return value
+//     }
+//     let processor: ProcessIdentity<number, string> = processIdentity;
+//     let returnNumber1 = processor(100, 'Hello!');   // OK
+//     // let returnString1 = processor('Hello!', 100);   // Type check error
+// --------------------------------------------------------------------
+// interface ProcessIdentity<T, U> {
+//     value: T;
+//     message: U;
+//     process(): T;
+// }
+// class processIdentity<X, Y> implements ProcessIdentity<X, Y> {
+//     value: X;
+//     message: Y;
+//     constructor(val: X, msg: Y) {
+//         this.value = val;
+//         this.message = msg;
+//     }
+//     process() : X {
+//         console.log(this.message);
+//         return this.value
+//     }
+// }
+// let processor = new processIdentity<number, string>(100, 'Hello');
+// processor.process();           // Displays 'Hello'
+// processor.value = '100';       // Type check error
+// --------------------------------------------------------------------
+class Car {
+    constructor() {
+        this.make = 'Generic Car';
+        this.doors = 4;
     }
 }
-let processor = new processIdentity(100, 'Hello');
-processor.process(); // Displays 'Hello'
-processor.value = '100'; // Type check error
+class ElectricCar extends Car {
+    constructor() {
+        super(...arguments);
+        this.make = 'Electric Car';
+        this.doors = 4;
+    }
+}
+class Truck extends Car {
+    constructor() {
+        super(...arguments);
+        this.make = 'Truck';
+        this.doors = 2;
+    }
+}
+function accelerate(car) {
+    console.log(`All ${car.doors} doors are closed.`);
+    console.log(`The ${car.make} is now accelerating!`);
+    return car;
+}
+let myElectricCar = new ElectricCar;
+accelerate(myElectricCar);
+let myTruck = new Truck;
+accelerate(myTruck);
--- module06.ts
+++ module06.ts
@@ -13,25 +13,50 @@
 
     // --------------------------------------------------------------------
 
-    interface ProcessIdentity<T, U> {
-        value: T;
-        message: U;
-        process(): T;
+    // interface ProcessIdentity<T, U> {
+    //     value: T;
+    //     message: U;
+    //     process(): T;
+    // }
+    
+    // class processIdentity<X, Y> implements ProcessIdentity<X, Y> {
+    //     value: X;
+    //     message: Y;
+    //     constructor(val: X, msg: Y) {
+    //         this.value = val;
+    //         this.message = msg;
+    //     }
+    //     process() : X {
+    //         console.log(this.message);
+    //         return this.value
+    //     }
+    // }
+    
+    // let processor = new processIdentity<number, string>(100, 'Hello');
+    // processor.process();           // Displays 'Hello'
+    // processor.value = '100';       // Type check error
+
+    // --------------------------------------------------------------------
+
+    class Car {
+        make: string = 'Generic Car';
+        doors: number = 4;
+    }
+    class ElectricCar extends Car {
+        make = 'Electric Car';
+        doors = 4;
+    }
+    class Truck extends Car {
+        make = 'Truck';
+        doors = 2;
+    }
+    function accelerate<T extends Car> (car: T): T {
+        console.log(`All ${car.doors} doors are closed.`);
+        console.log(`The ${car.make} is now accelerating!`)
+        return car
     }
     
-    class processIdentity<X, Y> implements ProcessIdentity<X, Y> {
-        value: X;
-        message: Y;
-        constructor(val: X, msg: Y) {
-            this.value = val;
-            this.message = msg;
-        }
-        process() : X {
-            console.log(this.message);
-            return this.value
-        }
-    }
-    
-    let processor = new processIdentity<number, string>(100, 'Hello');
-    processor.process();           // Displays 'Hello'
-    processor.value = '100';       // Type check error
+    let myElectricCar = new ElectricCar;
+    accelerate<ElectricCar>(myElectricCar);
+    let myTruck = new Truck;
+    accelerate<Truck>(myTruck);
